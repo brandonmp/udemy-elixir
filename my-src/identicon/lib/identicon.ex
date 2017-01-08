554b49e5c1bcb4e3586e274e1e9ad88f94ca8ca0
brandonmp <brandon@wayhome.io>
2017-01-08 11:14:25 -0500

    .

---
+++ my-src/identicon/lib/identicon.ex
@@ -0,0 +1,37 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello
+      :world
+
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+end
+
+({hex}) => {
+  const [r,g,b] = hex
+  return {r,g,b}
+}
